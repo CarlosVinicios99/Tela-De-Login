194b877f995187637ea84bd865803477a55766cc
Carlos Vinicios De Souza Azeredo <55165185+CarlosVinicios99@users.noreply.github.com>
2022-12-01 20:46:03 -0300

    Update TelaLogin.java

--- tela-de-login/src/TelaLogin.java
+++ tela-de-login/src/TelaLogin.java
@@ -8,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
  
-public class appFXML extends Application{
+public class TelaLogin extends Application{
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception{
